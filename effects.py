ba44ac0839223af2c12e8cf1fa49fe21c645d50d
Chaam Zeina <zeinachaam@gmail.com>
2026-03-03 07:38:59 +0000

    add moving arrows to git lines

--- effects.py
+++ effects.py
@@ -31,20 +31,70 @@ class GitLine(Effect):
         super().__init__(screen, **kwargs)
         self._x = x
         self._y = y
+        self._top_direction = 1
+        self._bottom_direction = -1
+        self._line = (
+            "--------------------------------------------------------------------"
+        )
+        self._arrow_pos_top = 0
+        self._arrow_pos_bottom = len(self._line) - 1
 
     def reset(self):
         pass
 
     def _update(self, frame_no):
-        line = "--------------------------------------------------------------------"
-        arrow_pos = (frame_no // 2) % len(line)
-        start_x = self._x - len(line) // 2
-        current_line = line[:arrow_pos] + ">" + line[arrow_pos + 1 :]
+        # control movement speed by frame number
+        if frame_no % 2 == 0:
+            self._arrow_pos_top += self._top_direction
+            self._arrow_pos_bottom += self._bottom_direction
+        # ensure arrow index within line bounds
+        self._arrow_pos_top = max(0, min(len(self._line) - 1, self._arrow_pos_top))
+        self._arrow_pos_bottom = max(
+            0, min(len(self._line) - 1, self._arrow_pos_bottom)
+        )
+        # render right arrow based on movement direction
+        arrow_top = ">" if self._top_direction == 1 else "<"
+        arrow_bottom = ">" if self._bottom_direction == 1 else "<"
+
+        start_x = self._x - len(self._line) // 2
+
+        # draw lines with arrow by replacing at position where arrow should currently be
+        current_line_top = (
+            self._line[: self._arrow_pos_top]
+            + arrow_top
+            + self._line[self._arrow_pos_top + 1 :]
+        )
+        current_line_bottom = (
+            self._line[: self._arrow_pos_bottom]
+            + arrow_bottom
+            + self._line[self._arrow_pos_bottom + 1 :]
+        )
+
+        # change direction only if hit bounds and going in wrong direction (otherwise get infinite flip at bounds)
+        if (
+            self._arrow_pos_bottom >= len(self._line) - 1
+            and self._bottom_direction == 1
+        ) or (self._arrow_pos_bottom <= 0 and self._bottom_direction == -1):
+            self._bottom_direction *= -1
+        if (
+            self._arrow_pos_top >= len(self._line) - 1 and self._top_direction == 1
+        ) or (self._arrow_pos_top <= 0 and self._top_direction == -1):
+            self._top_direction *= -1
+
+        # print lines w arrows to screen
         self._screen.print_at(
-            current_line, start_x, self._y + 5, Screen.COLOUR_GREEN, attr=Screen.A_BOLD
+            current_line_top,
+            start_x,
+            self._y + 5,
+            Screen.COLOUR_GREEN,
+            attr=Screen.A_BOLD,
         )
         self._screen.print_at(
-            current_line, start_x, self._y - 5, Screen.COLOUR_GREEN, attr=Screen.A_BOLD
+            current_line_bottom,
+            start_x,
+            self._y - 5,
+            Screen.COLOUR_GREEN,
+            attr=Screen.A_BOLD,
         )
 
     @property
