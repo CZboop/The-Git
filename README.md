255cf8e3539bf080d50640deb6a42436d3d29db8
Chaam Zeina <zeinachaam@gmail.com>
2026-03-08 18:37:22 +0000

    remove vhs workflow and .tape, add gif and update readme to include it

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # The Git Happens Here 🖥️
 
 Net art inspired ASCII terminal animation. Badly representing where the git happens 🤔
+
+Made using the great [asciimatics package](https://github.com/peterbrittain/asciimatics).
+
+## The Gif 📽️
+Check out the animation in action:
+![terminal animation of two computers with arrows moving between them, and text that says "the git happens here"](https://github.com/CZboop/The-Git/tree/main/media/the_git.gif)
+
+## Inspiration 💡
+Inspired by [this piece of art](https://rhizome.org/events/the-art-happens-here-net-art-anthology-launch/), that very helpfully illustrates exactly how Net Art works.
---
+++ src/main.py
@@ -0,0 +1,66 @@
+import sys
+
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+
+from src.effects import COMPUTER_SHAPE, CenterText, Computer, GitLine, Sparkles
+
+
+def the_git(screen: Screen) -> None:
+    """Main animation looping forever
+
+    Args:
+        screen (Screen): Screen passed from wrapper
+    """
+    center_x = screen.width // 2
+    center_y = screen.height // 2
+
+    # computer positions
+    computer_width = len(COMPUTER_SHAPE[0])
+    computer_height = len(COMPUTER_SHAPE)
+
+    positions = {
+        "top_left": (
+            center_x - int(computer_width * 2),
+            center_y - computer_height // 2,
+        ),
+        "top_right": (center_x + (computer_width), center_y - computer_height // 2),
+    }
+    # get computer centres to randomise sparkles roughly within their bounds
+    computer_centres = [
+        (
+            positions["top_left"][0] + computer_width // 2,
+            positions["top_left"][1] + computer_height // 2,
+        ),
+        (
+            positions["top_right"][0] + computer_width // 2,
+            positions["top_right"][1] + computer_height // 2,
+        ),
+    ]
+
+    effects = [
+        CenterText(screen, "THE GIT HAPPENS", center_y - computer_height + 2),
+        CenterText(screen, "HERE", center_y - 3),
+        Computer(screen, positions["top_left"][0], positions["top_left"][1]),
+        Computer(screen, positions["top_right"][0], positions["top_right"][1]),
+        GitLine(screen, center_x, center_y),
+        Sparkles(
+            screen,
+            positions=computer_centres,
+            radius=min(computer_height, computer_width),
+        ),
+    ]
+
+    scenes = [
+        Scene(effects, -1),
+    ]  # -1 means loop forever
+    screen.play(scenes, stop_on_resize=True)
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            Screen.wrapper(the_git)
+            sys.exit(0)
+        except Exception as e:
+            print(f"Something went wrong - {e}")
